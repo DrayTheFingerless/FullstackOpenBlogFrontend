682487b3742ed64698e67801d2d0e6b6bc43cdff
Rob <robert.ferreiraw90@gmail.com>
2024-11-07 17:45:39 +0000

    part 5, exercise 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div>
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (<div style={blogStyle}>
     {blog.title} - {blog.author}
-  </div>  
-)
+    <Togglable buttonLabel="show">
+    <p>{blog.title}</p>
+    <p>{blog.url}</p>
+    <p>{blog.likes}<button>Like</button></p>
+    <p>{blog.author}</p>
+    </Togglable>
+  </div>  )
+}
 
 export default Blog
