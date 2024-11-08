007e09150d014e70dbdd39dc0a562170fc010449
Rob <robert.ferreiraw90@gmail.com>
2024-11-08 14:03:55 +0000

    part 5, exercise 10

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs( blogs.sort((a, b) => a.likes - b.likes))
     )  
   }, [user])
 
